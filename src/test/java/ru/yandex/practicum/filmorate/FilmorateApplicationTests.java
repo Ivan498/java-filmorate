0029acf62f582db3a224e2c95f0aa10c77e4018d
Ivan <vankar2824@mail.ru>
2024-02-09 23:16:55 +0300

    Удалил импорты

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.filmorate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
-
 import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import javax.validation.Validation;
