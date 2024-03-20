4e8edd0c605f376624eb2307096c346d902a6acd
Ivan498 <vankar2824@mail.ru>
2024-03-20 16:36:03 +0300

    add database 3

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO film (id, name) VALUES
-                                (1, 'The Shawshank Redemption'),
-                                (2, 'The Godfather'),
-                                (3, 'The Dark Knight');
+                  (1, 'The Shawshank Redemption'),
+                  (2, 'The Godfather'),
+                  (3, 'The Dark Knight');
