99010ac428fb926de5bcf3b20f48d158347f5daa
Ivan498 <vankar2824@mail.ru>
2024-06-17 01:49:30 +0300

    fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/dao/repository/JdbcFilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/repository/JdbcFilmRepository.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.dao.repository;
 
-import lombok.RequiredArgsConstructor;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
--- src/main/java/ru/yandex/practicum/filmorate/dao/repository/JdbcLikeRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/repository/JdbcLikeRepository.java
@@ -1,5 +1,5 @@
 package ru.yandex.practicum.filmorate.dao.repository;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
